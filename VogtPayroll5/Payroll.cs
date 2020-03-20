7a08b637a92e2fd91a4420f971ab0b65bfad91e7
Gabriel <gvogt@asu.edu>
2020-03-20 10:32:05 -0700

    added static total gross pay and count of hours

--- VogtPayroll5/Payroll.cs
+++ VogtPayroll5/Payroll.cs
@@ -6,17 +6,21 @@ namespace VogtPayroll5
 {
     class Payroll
     {
+        //total number of hours worked by all employees, and total gross pay as well
+        private static int countHours;
+        private static decimal totalGrossPay;
+
         private List<Employee> _empList = new List<Employee>();
+
         public void Run()
         {
-            List<Employee> empList = new List<Employee>();
             PayrollConsoleReader payrollConsoleReader = new PayrollConsoleReader();
             Employee emp = new Employee();
             bool run = false;
 
             do
             {
-
+                Console.Clear();
                 emp = payrollConsoleReader.GetEmployeeInfo();
                 run = payrollConsoleReader.ReturnTrueOrFalse();
 
@@ -24,13 +28,31 @@ namespace VogtPayroll5
             } while (run == true);
 
             LoopThroughEmployeeInfo(emp.CalculateGrossPay(emp.HoursWorked, emp.HourlyPayRate));
+
+            DisplayTotalEmployeesHoursWorkedAndGrossPay();
+        }
+
+        public void DisplayTotalEmployeesHoursWorkedAndGrossPay()
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Employee count: {_empList.Count}");
+            Console.WriteLine($"Employee total of hours worked {countHours}");
+            Console.WriteLine($"Employee total of gross pay {totalGrossPay:C2}");
+
         }
 
         public void LoopThroughEmployeeInfo(decimal grossPay)
         {
+            Console.Clear();
             foreach (var employee in _empList)
             {
+                Console.WriteLine();
+
                 employee.DisplayPayStatement(employee.EmpID, employee.EmpName, employee.HoursWorked, employee.HourlyPayRate, grossPay);
+
+                countHours += employee.HoursWorked;
+                totalGrossPay += grossPay;
+
             }
         }
     }
